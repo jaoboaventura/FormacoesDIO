e69f401127b118d62dcd79df4544b94e205707be
Joao <jp.boaventura99@hotmail.com>
2024-10-04 11:54:05 -0300

    Adicionando a pasta desafio01-calculadora sem submódulo

---
+++ FormacaoReact/desafio01-calculadora/src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background: #F24405;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: "Poppins", sans-serif;
+
+  input {
+    width: 100%;
+    height: 75px;
+    border: 0;
+    padding: 0 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    
+    background: #F24405;
+    color: #F2F2F2;
+    font-size: 24px;
+    font-family: "Poppins", sans-serif;
+  }
+`;
---
+++ FormacaoReact/desafio01-calculadora/src/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: #262626;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background: #262626;
+    width: 50%;
+    min-height: 350px;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
